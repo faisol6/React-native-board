baeebbb0eff4faf4a6f1f47b7a6cb453bc7dd6b9
faisorn6 <faisorn6@gmail.com>
2024-01-29 04:27:52 +0700

    finish board

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import { Button } from "react-native";
 import HomeScreen from "./Home";
 import Board from "./Board";
+import storage from "./Storeage";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,8 +11,20 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Board" component={Board} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={({ navigation }) => ({
+            title: "QUESTION",
+          })}
+        />
+        <Stack.Screen
+          name="Board"
+          component={Board}
+          options={({ navigation }) => ({
+            title: "SCOREBOARD",
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Board.tsx
+++ Board.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { SafeAreaView, StyleSheet, View, Text, StatusBar } from "react-native";
+import { ScrollView, StyleSheet, View, Button } from "react-native";
 import storage from "./Storeage";
+import { DataTable } from "react-native-paper";
 
 const Board = ({ navigation, route }: any) => {
   const [scoreList, setScoreList] = useState<{ name: string; score: number }[]>(
@@ -17,8 +18,9 @@ const Board = ({ navigation, route }: any) => {
         const reList = [
           ...res,
           { name: route.params.name, score: route.params.score },
-        ];
-        console.log(reList);
+        ]?.sort((a, b) => {
+          return b.score - a.score;
+        });
         setScoreList(reList);
         storage.save({
           key: "scoreBoard",
@@ -34,17 +36,72 @@ const Board = ({ navigation, route }: any) => {
               id: "1",
               data: [{ name: route.params.name, score: route.params.score }],
             });
+            setScoreList([
+              { name: route.params.name, score: route.params.score },
+            ]);
             break;
         }
       });
   }, []);
 
   return (
-    <Text>
-      {route.params.name}
-      {route.params.score}
-    </Text>
+    <View>
+      <DataTable style={styles.container}>
+        <DataTable.Header style={styles.tableHeader}>
+          <DataTable.Title>Name</DataTable.Title>
+          <DataTable.Title
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            Score
+          </DataTable.Title>
+        </DataTable.Header>
+        <ScrollView>
+          {scoreList?.map(
+            (
+              sc: {
+                name: string;
+                score: number;
+              },
+              idx: number
+            ) => {
+              return (
+                <DataTable.Row key={idx}>
+                  <DataTable.Cell>{sc.name}</DataTable.Cell>
+
+                  <DataTable.Cell
+                    style={{ display: "flex", justifyContent: "center" }}
+                  >
+                    {sc.score}
+                  </DataTable.Cell>
+                </DataTable.Row>
+              );
+            }
+          )}
+        </ScrollView>
+      </DataTable>
+      <Button
+        color={"red"}
+        title="Clear Scoreboard"
+        onPress={() => {
+          storage.remove({
+            key: "scoreBoard",
+            id: "1",
+          });
+          setScoreList([]);
+        }}
+      />
+    </View>
   );
 };
 
 export default Board;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+    minHeight: "95%",
+  },
+  tableHeader: {
+    backgroundColor: "#DCDCDC",
+  },
+});
